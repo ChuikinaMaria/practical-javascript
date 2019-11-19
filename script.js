b52323b36d42166f39aeb67decc528dd6b5a8d46
Masha <4uikina@gmail.com>
2019-11-19 21:02:28 +0100

    v4

--- script.js
+++ script.js
@@ -1,15 +1,22 @@
 let todoList = {
-    todos: ['item 1', 'item 2', 'item 3'],
+    todos: [],
     displayTodos: function() {console.log(this.todos)},
-    addTodo: function(newTodos){
-        this.todos.push(newTodos);
+    addTodo: function(name){
+        this.todos.push({
+            name: 'newTodos',
+            completed: false});
         this.displayTodos()},
     changeTodo: function(num, name) {
-        this.todos[num] = name;
+        this.todos[num].newTodos = name;
         this.displayTodos()},
     deleteTodo: function(num){
         this.todos.splice(num, 1);
         this.displayTodos();},
+    completed: function(num){
+        let a = this.todos[num].completed
+        this.todos[num].completed = !a;
+        this.displayTodos();
+    }
 
 
 
