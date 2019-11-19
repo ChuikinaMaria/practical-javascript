ee5a1367887552490134fd5232340386025d4530
Masha <4uikina@gmail.com>
2019-11-19 14:39:20 +0100

    v3

--- script.js
+++ script.js
@@ -1,15 +1,16 @@
 let todoList = {
     todos: ['item 1', 'item 2', 'item 3'],
     displayTodos: function() {console.log(this.todos)},
-    addTodos: function(newTodos) {this.todos.push(newTodos);
-    this.displayTodos()},
-    changeTodos: function(num, name) {
+    addTodo: function(newTodos){
+        this.todos.push(newTodos);
+        this.displayTodos()},
+    changeTodo: function(num, name) {
         this.todos[num] = name;
         this.displayTodos()},
-    deleteTodos: function(num){
-        delete this.todos[num];
-        this.displayTodos();
-    }
+    deleteTodo: function(num){
+        this.todos.splice(num, 1);
+        this.displayTodos();},
+
 
 
 }
